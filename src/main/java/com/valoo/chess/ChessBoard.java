a4b9dedd5a98703d6a02fde3b01f48d117a4d911
BALDONI-Jules-2326013aa <jules.baldoni@etu.univ-amu.fr>
2024-06-04 11:19:18 +0200

    Fin des mouvements

--- src/main/java/com/valoo/chess/ChessBoard.java
+++ src/main/java/com/valoo/chess/ChessBoard.java
@@ -8,6 +8,8 @@ import javafx.scene.layout.VBox;
 
 public class ChessBoard {
     private VBox board;
+    private Piece[][] matPiece;
+    private Piece selectedPiece;
 
     public ChessBoard() {
         board = new VBox();
@@ -26,125 +28,92 @@ public class ChessBoard {
                 } else {
                     square.setStyle("-fx-background-color: #EEEED5");
                 }
+                int x = j;
+                int y = i;
+                square.setOnMouseClicked(event -> handleSquareClick(x, y));
                 row.getChildren().add(square);
             }
             board.getChildren().add(row);
         }
     }
 
+    private void handleSquareClick(int x, int y) {
+        if (selectedPiece == null) {
+            selectPiece(x, y);
+        } else {
+            movePiece(selectedPiece.getX(), selectedPiece.getY(), x, y);
+            selectedPiece = null;
+            updateBoard();
+            colorBoard();
+        }
+    }
+
     private void placePieces() {
-        Piece[][] pieces = {
+        matPiece = new Piece[][]{
                 {new Tour("blanc", "tour", 0, 0, 0), new Cavalier("blanc", "cavalier", 0, 1, 0), new Fou("blanc", "fou", 0, 2, 0), new Reine("blanc", "reine", 0, 3, 0), new Roi("blanc", "roi", 0, 4, 0), new Fou("blanc", "fou", 0, 5, 0), new Cavalier("blanc", "cavalier", 0, 6, 0), new Tour("blanc", "tour", 0, 7, 0)},
                 {new Pion("blanc", "pion", 0, 0, 1), new Pion("blanc", "pion", 0, 1, 1), new Pion("blanc", "pion", 0, 2, 1), new Pion("blanc", "pion", 0, 3, 1), new Pion("blanc", "pion", 0, 4, 1), new Pion("blanc", "pion", 0, 5, 1), new Pion("blanc", "pion", 0, 6, 1), new Pion("blanc", "pion", 0, 7, 1)},
-                {null,null, null, null, null, null, null, null},
-                {null, null,null, null, null,  new Reine("noir", "reine", 1, 5, 3), null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, new Reine("noir", "reine", 1, 5, 3), null, null},
                 {null, null, null, null, null, null, null, null},
                 {null, null, null, null, null, null, null, null},
                 {new Pion("noir", "pion", 1, 0, 6), new Pion("noir", "pion", 1, 1, 6), new Pion("noir", "pion", 1, 2, 6), new Pion("noir", "pion", 1, 3, 6), new Pion("noir", "pion", 1, 4, 6), new Pion("noir", "pion", 1, 5, 6), new Pion("noir", "pion", 1, 6, 6), new Pion("noir", "pion", 1, 7, 6)},
                 {new Tour("noir", "tour", 1, 0, 7), new Cavalier("noir", "cavalier", 1, 1, 7), new Fou("noir", "fou", 1, 2, 7), new Reine("noir", "reine", 1, 3, 7), new Roi("noir", "roi", 1, 4, 7), new Fou("noir", "fou", 1, 5, 7), new Cavalier("noir", "cavalier", 1, 6, 7), new Tour("noir", "tour", 1, 7, 7)}
         };
 
+        updateBoard();
+    }
+
+    private void updateBoard() {
         for (int i = 0; i < 8; i++) {
             HBox row = (HBox) board.getChildren().get(i);
             for (int j = 0; j < 8; j++) {
                 StackPane square = (StackPane) row.getChildren().get(j);
-                Piece piece = pieces[i][j];
+                square.getChildren().clear();
+                Piece piece = matPiece[i][j];
                 if (piece != null) {
                     ImageView pieceView = new ImageView(piece.getImagePiece());
                     pieceView.setFitWidth(90);
                     pieceView.setFitHeight(90);
                     pieceView.setUserData(piece);
                     square.getChildren().add(pieceView);
-
-                    square.setOnMouseClicked(event -> {
-
-                        colorBoard();
-
-                        Piece clickedPiece = (Piece) pieceView.getUserData();
-                        int[][] validMoves = clickedPiece.validMoves(this);
-
-                        for (int[] move : validMoves) {
-                            HBox targetRow = (HBox) board.getChildren().get(move[1]);
-                            StackPane targetSquare = (StackPane) targetRow.getChildren().get(move[0]);
-                            targetSquare.setStyle("-fx-background-color: #E2C64B");
-                        }
-
-                        for(int k = 0 ; k < validMoves.length ; k++) {
-                            int[] move = validMoves[k];
-                            int x = move[0];
-                            int y = move[1];
-                            StackPane targetSquare = (StackPane) ((HBox) board.getChildren().get(y)).getChildren().get(x);
-                            targetSquare.setOnMouseClicked(event1 -> {
-                                movePiece(clickedPiece.getX(), clickedPiece.getY(), x, y);
-                                colorBoard();
-                            });
-                        }
-                    });
-
-                    }
+                }
             }
         }
     }
 
-    // methode getPiece qui renvoie la piece aux coordonnées x et y fournies en paramètre
     public Piece getPiece(int x, int y) {
-        HBox row = (HBox) board.getChildren().get(y);
-        StackPane square = (StackPane) row.getChildren().get(x);
-
-        // Vérifier si la case est vide
-        if (square.getChildren().size() == 0) {
-            return null;
-        }
-
-        // Vérifier si la pièce est représentée par un ImageView
-        Node pieceNode = square.getChildren().get(0);
-        if (!(pieceNode instanceof ImageView)) {
-            return null;
-        }
-        // Récupérer la pièce à partir des données utilisateur de l'ImageView
-        ImageView pieceView = (ImageView) pieceNode;
-        return (Piece) pieceView.getUserData();
+        return matPiece[y][x];
     }
 
-    //selectPiece
-    // Méthode pour sélectionner une pièce au premier clic
     public void selectPiece(int x, int y) {
         Piece piece = getPiece(x, y);
         if (piece != null) {
-            System.out.println("Piece selected: " + piece);
-            // Stockez les coordonnées de la pièce sélectionnée ou effectuez d'autres actions si nécessaire
-        } else {
-            System.out.println("No piece at position [" + x + ", " + y + "]");
+            selectedPiece = piece;
+            highlightValidMoves(piece);
         }
     }
 
-
+    private void highlightValidMoves(Piece piece) {
+        int[][] validMoves = piece.validMoves(this);
+        for (int[] move : validMoves) {
+            int x = move[0];
+            int y = move[1];
+            StackPane targetSquare = (StackPane) ((HBox) board.getChildren().get(y)).getChildren().get(x);
+            targetSquare.setStyle("-fx-background-color: #E2C64B");
+        }
+    }
 
     public boolean movePiece(int currentX, int currentY, int targetX, int targetY) {
         Piece piece = getPiece(currentX, currentY);
         if (piece != null) {
-            int[][] validMoves = piece.validMoves(this); // Obtenez les mouvements valides de la pièce
+            int[][] validMoves = piece.validMoves(this);
             for (int[] move : validMoves) {
                 if (move[0] == targetX && move[1] == targetY) {
-                    // Déplacez la pièce et mettez à jour l'image
-                    HBox currentRow = (HBox) board.getChildren().get(currentY);
-                    StackPane currentSquare = (StackPane) currentRow.getChildren().get(currentX);
-                    currentSquare.getChildren().clear(); // Supprimez l'image de la case actuelle
-
-                    HBox targetRow = (HBox) board.getChildren().get(targetY);
-                    StackPane targetSquare = (StackPane) targetRow.getChildren().get(targetX);
-                    ImageView pieceView = new ImageView(piece.getImagePiece());
-                    pieceView.setFitWidth(90);
-                    pieceView.setFitHeight(90);
-                    pieceView.setUserData(piece);
-                    targetSquare.getChildren().add(pieceView); // Déplacez l'image vers la case cible
-
-                    // Mettez à jour les coordonnées de la pièce
+                    matPiece[targetY][targetX] = piece;
+                    matPiece[currentY][currentX] = null;
                     piece.setX(targetX);
                     piece.setY(targetY);
-
-                    System.out.println("Piece moved from [" + currentX + ", " + currentY + "] to [" + targetX + ", " + targetY + "]");
-                    return false;
+                    return true;
                 }
             }
             System.out.println("Invalid move for piece at [" + currentX + ", " + currentY + "]");
